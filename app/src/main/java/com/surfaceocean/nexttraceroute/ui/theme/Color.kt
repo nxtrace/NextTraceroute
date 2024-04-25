97bc6c691ba118caae4d860e7fb5ed5bed149a87
surfaceocean <96903179+surfaceocean@users.noreply.github.com>
2024-04-26 00:29:40 +0800

    initial commit

---
+++ app/src/main/java/com/surfaceocean/nexttraceroute/ui/theme/Color.kt
@@ -0,0 +1,45 @@
+/*
+
+NextTraceroute, an Android traceroute app using Nexttrace API
+Copyright (C) 2024 surfaceocean
+Email: r2qb8uc5@protonmail.com
+GitHub: https://github.com/nxtrace/NextTraceroute
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This project uses the libraries listed below. Detailed information can be found in the LICENSE file of this project.
+The "dnsjava" library is licensed under the BSD 3-Clause License.
+The "seancfoley/IPAddress" library is licensed under the Apache 2.0 License.
+The "square/okhttp" library is licensed under the Apache 2.0 License.
+The "gson" library is licensed under the Apache 2.0 License.
+The "slf4j-android" library is licensed under the MIT License.
+The "androidx" library is licensed under the Apache 2.0 License.
+
+*/
+package com.surfaceocean.nexttraceroute.ui.theme
+
+
+import androidx.compose.ui.graphics.Color
+
+
+val DefaultBackgroundColor= Color.Black
+val DefaultBackgroundColorReverse= Color.White
+val ButtonEnabledColor = Color(0xFF00F6FF)
+val ButtonDisabledColor = Color.Gray
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
