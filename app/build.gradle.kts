a65a79daa113ad395794edd334b972885fe6244a
surfaceocean <96903179+surfaceocean@users.noreply.github.com>
2024-08-20 09:58:04 +0800

    test v0.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.surfaceocean.nexttraceroute"
         minSdk = 21
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.0.8"
+        versionCode = 9
+        versionName = "0.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
