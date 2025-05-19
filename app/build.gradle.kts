8e58f467963517ca5f68a65fb7a50763316c763d
surfaceocean <96903179+surfaceocean@users.noreply.github.com>
2025-05-19 21:21:58 +0800

    v0.1.4 update android:configChanges to prevent recomposes from screen rotation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.surfaceocean.nexttraceroute"
         minSdk = 21
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.1.3"
+        versionCode = 14
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
